915a25e0406b4a21d0bbacdba20381e56cc1a168
demons <837940593@qq.com>
2017-04-29 12:15:59 +0000

    .

---
+++ lab2.1/Code/4.c
@@ -0,0 +1,49 @@
+#include "SDTAction.h"
+#include <malloc.h>
+
+/* FuncParamType */
+/*     : Specifier DEDUCT FuncParamType { */ 
+/*         $$ = new_parent_node("FuncType", GROUP_4 + 1, 2, $1, $3); */ 
+/*         $$ = new_parent_node("Specifier", GROUP_8 + 6, 1, $$); */
+/*     } */
+/*     | Specifier { $$ = $1; } */
+/*     ; */
+ID(401)
+{
+}
+
+/* FuncType */
+/*     : FUNC LP FuncParamType RP { */ 
+/*         if (strcmp(((AST_node *)(((AST_node *)$3)->first_child))->str, "FuncType")) */
+/*         { */
+/*             $$ = new_parent_node("FuncType", GROUP_4 + 2, 1, $3); */
+/*             $$ = new_parent_node("Specifier", GROUP_8 + 6, 1, $$); */
+/*         } */
+/*         else */ 
+/*         { */
+/*             $$ = $3; */
+/*         } */
+/*     } */
+/*     ; */
+/* FuncDec */
+/*     : LP VarList RP DEDUCT Specifier { $$ = new_parent_node("FuncDec", GROUP_4 + 3, 2, $2, $5); } */
+/*     | LP RP DEDUCT Specifier { $$ = new_parent_node("FuncDec", GROUP_4 + 4, 1, $4); } */
+/*     ; */
+/* VarList */
+/*     : ParamDec COMMA VarList { $$ = new_parent_node("VarList", GROUP_4 + 5, 2, $1, $3); } */
+/*     | ParamDec { $$ = new_parent_node("VarList", GROUP_4 + 6, 1, $1); } */
+/*     ; */
+/* ParamDec */
+/*     : Specifier VarDec { $$ = new_parent_node("ParamDec", GROUP_4 + 7, 2, $1, $2); } */
+/*     ; */
+/* FuncBody */
+/*     : FuncDec CompSt { $$ = new_parent_node("FuncBody", GROUP_4 + 8, 2, $1, $2); } */
+/*     ; */
+/* FuncCall */
+/*     : Exp LP Args RP { $$ = new_parent_node("FuncCall", GROUP_4 + 9, 2, $1, $3); } */
+/*     ; */
+/* Args */
+/*     : Exp COMMA Args { $$ = new_parent_node("Args", GROUP_4 + 10, 2, $1, $3); } */
+/*     | PLACEHOLDER COMMA Args { $$ = new_parent_node("Args", GROUP_4 + 11, 2, $1, $3); } */
+/*     | /1* empty *1/ { $$ = new_parent_node("Args", GROUP_4 + 12, 0); } */
+/*     ; */
