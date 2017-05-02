8ea157d065fce2a6de51d85610f62827b6b9c755
demons <837940593@qq.com>
2017-05-02 03:54:27 +0000

    handle ADT param now.

--- lab2.1/Code/7.c
+++ lab2.1/Code/7.c
@@ -7,6 +7,14 @@
 /*     : ADTHeader %prec LOWER_THAN_ASSIGNOP SEMI { $$ = new_parent_node("ADTRef", GROUP_7 + 1, 1, $1); } */
 /*     | ADTHeader ASSIGNOP ConstructorDecList { $$ = new_parent_node("ADTHeader", GROUP_7 + 2, 2, $1, $3); } */
 /*     ; */
+ID(702)
+{
+    if (childNum == 2)
+    {
+        D_child_1_info;
+        child->other_info = child_1_info;
+    }
+}
 /* ADTHeader */
 /*     : DATA TypeId ADTParamList { $$ = new_parent_node("ADTHeader", GROUP_7 + 3, 2, $2, $3); } */
 /*     ; */
@@ -18,6 +26,11 @@ ID(703)
         /* child->oter_info充当继承属性 */
         child->other_info = child_1_info;
     }
+}
+SD(703)
+{
+    D_child_1_info;
+    parent->other_info = child_1_info;
 }
 /* ADTParamList */
 /*     : ADTParam ADTParamList { $$ = new_parent_node("ADTParamList", GROUP_7 + 4, 2, $1, $2); } */
@@ -52,9 +65,33 @@ SD(706)
 /*     : ConstructorDec SEMI { $$ = new_parent_node("ConstructorDecList", GROUP_7 + 7, 1, $1); } */
 /*     | ConstructorDec SEMI ConstructorDecList { $$ = new_parent_node("ConstructorDecList", GROUP_7 + 8, 2, $1, $3); } */ 
 /*     ; */
+ID(707)
+{
+    if (childNum == 1)
+    {
+        D_parent_info;
+        child->other_info = parent_info;
+    }
+}
+ID(708)
+{
+    if (childNum == 1 || childNum == 3)
+    {
+        D_parent_info;
+        child->other_info = parent_info;
+    }
+}
 /* ConstructorDec */
 /*     : ConstructorId TypeIdList { $$ = new_parent_node("ConstructorDec", GROUP_7 + 9, 2, $1, $2); } */
 /*     ; */
+ID(709)
+{
+    if (childNum == 2)
+    {
+        D_parent_info;
+        child->other_info = parent_info;
+    }
+}
 /* ConstructorId */
 /*     : UPPERID { $$ = new_parent_node("ConstructorId", GROUP_7 + 10, 1, $1); } */
 /*     ; */
@@ -68,6 +105,14 @@ SD(710)
 /*     | ADTParam TypeIdList { $$ = new_parent_node("TypeIdList", GROUP_7 + 12, 2, $1, $2); } */
 /*     | /1* empty *1/ { $$ = new_parent_node("TypeIdList", GROUP_7 + 13, 0); } */
 /*     ; */
+ID(712)
+{
+    if (childNum == 1 || childNum == 2)
+    {
+        D_parent_info;
+        child->other_info = parent_info;
+    }
+}
 /* TypeId */
 /*     : UPPERID { $$ = new_parent_node("TypeId", GROUP_7 + 14, 1, $1); } */
 /*     ; */
@@ -97,6 +142,6 @@ SD(714)
 
 void initActionTable7()
 {
-    IS(703, 704);
-    SS(706, 710, 714);
+    IS(702, 703, 704, 707, 708, 709, 712);
+    SS(703, 706, 710, 714);
 }
