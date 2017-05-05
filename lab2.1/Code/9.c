89fac993a05d8a7fb9b14561f29f545a3318086c
demons <837940593@qq.com>
2017-05-05 11:20:50 +0000

    pass the first step

--- lab2.1/Code/9.c
+++ lab2.1/Code/9.c
@@ -6,6 +6,30 @@
 /*     : Specifier DecList { $$ = new_parent_node("VarDef", GROUP_9 + 1, 2, $1, $2); } */
 /*     | PatternMatching { $$ = new_parent_node("VarDef", GROUP_9 + 2, 1, $1); } */
 /*     ; */
+ID(901)
+{
+    if (childNum == 2)
+    {
+        D_child_1_info;
+        assert(child_1_info != NULL);
+        assert(child_1_info->typeKind != 0);
+        assert(child_1_info->node != NULL);
+        assert(child_1_info->nextInfo == NULL);
+
+        assert(child->other_info == NULL);
+        child->other_info = child_1_info;
+
+        D_child_1;
+        child_1->other_info = NULL;
+
+#ifdef exp_type_debug_print
+        D_child_2_info;
+        printTypeInfo(child_2_info);
+        printf("(ID(901))\n");
+#endif
+    }
+}
+
 /* DecList */
 /*     : Dec { $$ = new_parent_node("DecList", GROUP_9 + 3, 1, $1); } */
 /*     | Dec COMMA DecList { $$ = new_parent_node("DecList", GROUP_9 + 4, 2, $1, $3); } */
@@ -49,6 +73,6 @@ SD(906)
 
 void initActionTable9()
 {
-    /* registerSAction(906, pro906SAction); */
+    IS(901);
     SS(904, 905, 906);
 }
