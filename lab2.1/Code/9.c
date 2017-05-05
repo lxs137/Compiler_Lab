de335f95aa66693ee6153cb4204d916f6bdb482e
demons <837940593@qq.com>
2017-05-05 13:21:40 +0000

    better 9.c

--- lab2.1/Code/9.c
+++ lab2.1/Code/9.c
@@ -17,17 +17,21 @@ ID(901)
         assert(child_1_info->nextInfo == NULL);
 
         assert(child->other_info == NULL);
+        /* 分配的资源由SD(901)回收 */
         child->other_info = child_1_info;
-
-        D_child_1;
-        child_1->other_info = NULL;
-
-#ifdef exp_type_debug_print
-        D_child_2_info;
-        printTypeInfo(child_2_info);
-        printf("(ID(901))\n");
-#endif
     }
+}
+SD(901)
+{
+    D_child_1;
+    assert(child_1->other_info != NULL);
+    /* 回收SD(801)-SD(805)分配的资源 */
+    child_1->other_info = NULL;
+
+    D_child_2;
+    assert(child_2->other_info != NULL);
+    /* 回收ID(901)回收的资源 */
+    child_2->other_info = NULL;
 }
 
 /* DecList */
@@ -45,16 +49,16 @@ ID(903)
         assert(parent_info->nextInfo == NULL);
 
         assert(child->other_info == NULL);
+        /* 分配的资源由SD(903)回收 */
         child->other_info = parent_info;
-
-        parent->other_info = NULL;
-
-#ifdef exp_type_debug_print
-        D_child_info;
-        printTypeInfo(child_info);
-        printf("(ID(903))\n");
-#endif
     }
+}
+SD(903)
+{
+    D_child_1;
+    assert(child_1->other_info != NULL);
+    /* 回收由ID(903)分配的资源 */
+    child_1->other_info = NULL;
 }
 
 ID(904)
@@ -68,19 +72,21 @@ ID(904)
         assert(parent_info->nextInfo == NULL);
 
         assert(child->other_info == NULL);
+        /* 分配的资源由SD(904)回收 */
         child->other_info = parent_info;
-
-#ifdef exp_type_debug_print
-        D_child_info;
-        printTypeInfo(child_info);
-        printf("(ID(904))\n");
-#endif
     }
+}
+SD(904)
+{
+    D_child_1;
+    assert(child_1->other_info != NULL);
+    /* 回收ID(904)分配的资源 */
+    child_1->other_info = NULL;
 
-    if (childNum == 2)
-    {
-        parent->other_info = NULL;
-    }
+    D_child_2;
+    assert(child_2->other_info != NULL);
+    /* 回收ID(904)分配的资源 */
+    child_2->other_info = NULL;
 }
 
 /* Dec */
@@ -92,6 +98,13 @@ ID(905)
     if (childNum == 1)
     {
         D_parent_info;
+        assert(parent_info != NULL);
+        assert(parent_info->typeKind != 0);
+        assert(parent_info->node != NULL);
+        assert(parent_info->nextInfo == NULL);
+
+        assert(child->other_info == NULL);
+        /* 分配的资源不回收直到语法树销毁 */
         child->other_info = parent_info;
     }
 }
@@ -101,18 +114,29 @@ SD(905)
     D_child_1_info;
     assert(child_1_info != NULL);
     addSymbol((char *)child_1_info, child_1);
-    /* 回收SD(906)分配的资源 */;
-    child_1->other_info = NULL;
 }
 
+ID(906)
+{
+    if (childNum == 1)
+    {
+        D_parent_info;
+        assert(parent_info != NULL);
+        assert(parent_info->typeKind != 0);
+        assert(parent_info->node != NULL);
+        assert(parent_info->nextInfo == NULL);
+
+        assert(child->other_info == NULL);
+        /* 分配的资源不回收直到语法树销毁 */
+        child->other_info = parent_info;
+    }
+}
 SD(906)
 {
     D_child_1;
     D_child_1_info;
     assert(child_1_info != NULL);
     addSymbol((char *)child_1_info, child_1);
-    /* 回收SD(906)分配的资源 */;
-    child_1->other_info = NULL;
 }
 
 /* Declarators */
@@ -121,11 +145,20 @@ SD(906)
 /*     ; */
 SD(907)
 {
+    D_parent_info;
+    assert(parent_info != NULL);
+    assert(parent_info->typeKind != 0);
+    assert(parent_info->nextInfo == NULL);
+
     D_child_1;
-    /* assert(parent->other_info == NULL); */
-    /* 分配的资源由SD(904) / SD(905) / SD(1016)回收 */
-    /* parent->other_info = child_1->str + 4; */
-    ((TypeInfo *)parent->other_info)->nextInfo = child_1->str + 4;
+    /* 分配的资源不回收直到语法树销毁 */
+    parent_info->nextInfo = child_1->str + 4;
+
+#ifdef exp_type_debug_print
+    printTypeInfo(parent_info);
+    printf(" (id: %s)", (char *)parent_info->nextInfo);
+    printf(" (SD(907))\n");
+#endif
 }
 
 void initActionTable9()
