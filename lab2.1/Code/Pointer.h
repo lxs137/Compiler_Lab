73afc6f7b92a019e7fec893d56dc89f9f56c1838
demons <837940593@qq.com>
2017-05-08 04:01:11 +0000

    pass compile?

--- lab2.1/Code/Pointer.h
+++ lab2.1/Code/Pointer.h
@@ -1,6 +1,8 @@
 #ifndef Pointer_H
 #define Pointer_H
 
+#include <stdarg.h>
+
 void allocPointer(int proNum, void **pointer, void *value, int isTypeInfo, int expectDeallocProNum, ...);
 void deallocPointer(int proNum, void **pointer);
 
