7be2bc378fd52a59482111dae5c175f8462cde7c
demons <837940593@qq.com>
2017-05-08 05:16:08 +0000

    pass tests?

--- lab2.1/Code/Pointer.c
+++ lab2.1/Code/Pointer.c
@@ -22,67 +22,77 @@ typedef struct PL
 
 static PointerLog *pl = NULL;
 
-void alloc(int proNum, void **pointer, void *value, int isTypeInfo, int expectDeallocProNum, ...)
+void alloc(int proNum, void **pointer, void *value)
 {
-    va_list argptr;
-    va_start(argptr, expectDeallocProNum);
-    /* assert(expectDeallocProNum != 0); */
-    ExpectProNum *ep = (ExpectProNum *)malloc(sizeof(ExpectProNum));
-    ep->proNum = expectDeallocProNum;
-    ExpectProNum *tmp = ep;
-    while (expectDeallocProNum != 0)
-    {
-	expectDeallocProNum = va_arg(argptr, int);
-	tmp->next = (ExpectProNum *)malloc(sizeof(ExpectProNum));
-	tmp = tmp->next;
-	tmp->proNum = expectDeallocProNum;
-    }
-    tmp->next = NULL;
-
-    /* assert(value != NULL); */
-    /* assert(*pointer == NULL); */
     *pointer = value;
-
-    PointerLog *newPl = (PointerLog *)malloc(sizeof(PointerLog));
-    newPl->allocProNum = proNum;
-    newPl->expectDeallocProNums = ep;
-    /* 不存在0号产生式，用0代表未赋值 */
-    newPl->deallocProNum = 0;
-    newPl->isTypeInfo = isTypeInfo;
-    newPl->nextPointerLog = NULL;
-
-    if (pl == NULL)
-    {
-	pl = newPl;
-    }
-    else
-    {
-	pl->nextPointerLog = newPl;
-    }
 }
 
 void dealloc(int proNum, void **pointer)
 {
-    PointerLog *tmp;
-    for (tmp = pl; tmp != NULL; tmp = tmp->nextPointerLog)
-    {
-	if (tmp->pointer == pointer)
-	{
-	    ExpectProNum *ep = tmp->expectDeallocProNums;
-	    while (ep == NULL || ep->proNum == proNum) ;
-	    /* assert(ep != NULL); */
-	    /* assert(tmp->deallocProNum == 0); */
-	    tmp->deallocProNum = proNum;
-
-	    /* assert(*pointer != NULL); */
-	    /* if (tmp->isTypeInfo == 1) */
-	    /* { */
-		/* TypeInfo *info = *((TypeInfo **)tmp); */
-		/* assert(info->nextInfo == NULL); */
-	    /* } */
-	    *pointer = NULL;
-	    return;
-	}
-    }
-    /* assert(0); */
+    *pointer = NULL;
 }
+
+/* void alloc(int proNum, void **pointer, void *value, int isTypeInfo, int expectDeallocProNum, ...) */
+/* { */
+/*     va_list argptr; */
+/*     va_start(argptr, expectDeallocProNum); */
+/*     /1* assert(expectDeallocProNum != 0); *1/ */
+/*     ExpectProNum *ep = (ExpectProNum *)malloc(sizeof(ExpectProNum)); */
+/*     ep->proNum = expectDeallocProNum; */
+/*     ExpectProNum *tmp = ep; */
+/*     while (expectDeallocProNum != 0) */
+/*     { */
+/* 	expectDeallocProNum = va_arg(argptr, int); */
+/* 	tmp->next = (ExpectProNum *)malloc(sizeof(ExpectProNum)); */
+/* 	tmp = tmp->next; */
+/* 	tmp->proNum = expectDeallocProNum; */
+/*     } */
+/*     tmp->next = NULL; */
+
+/*     /1* assert(value != NULL); *1/ */
+/*     /1* assert(*pointer == NULL); *1/ */
+/*     *pointer = value; */
+
+/*     PointerLog *newPl = (PointerLog *)malloc(sizeof(PointerLog)); */
+/*     newPl->allocProNum = proNum; */
+/*     newPl->expectDeallocProNums = ep; */
+/*     /1* 不存在0号产生式，用0代表未赋值 *1/ */
+/*     newPl->deallocProNum = 0; */
+/*     newPl->isTypeInfo = isTypeInfo; */
+/*     newPl->nextPointerLog = NULL; */
+
+/*     if (pl == NULL) */
+/*     { */
+/* 	pl = newPl; */
+/*     } */
+/*     else */
+/*     { */
+/* 	pl->nextPointerLog = newPl; */
+/*     } */
+/* } */
+
+/* void dealloc(int proNum, void **pointer) */
+/* { */
+/*     PointerLog *tmp; */
+/*     for (tmp = pl; tmp != NULL; tmp = tmp->nextPointerLog) */
+/*     { */
+/* 	if (tmp->pointer == pointer) */
+/* 	{ */
+/* 	    ExpectProNum *ep = tmp->expectDeallocProNums; */
+/* 	    while (ep == NULL || ep->proNum == proNum) ; */
+/* 	    /1* assert(ep != NULL); *1/ */
+/* 	    /1* assert(tmp->deallocProNum == 0); *1/ */
+/* 	    tmp->deallocProNum = proNum; */
+
+/* 	    /1* assert(*pointer != NULL); *1/ */
+/* 	    /1* if (tmp->isTypeInfo == 1) *1/ */
+/* 	    /1* { *1/ */
+/* 		/1* TypeInfo *info = *((TypeInfo **)tmp); *1/ */
+/* 		/1* assert(info->nextInfo == NULL); *1/ */
+/* 	    /1* } *1/ */
+/* 	    *pointer = NULL; */
+/* 	    return; */
+/* 	} */
+/*     } */
+/*     /1* assert(0); *1/ */
+/* } */
--- lab2.1/Code/Pointer.h
+++ lab2.1/Code/Pointer.h
@@ -3,7 +3,11 @@
 
 #include <stdarg.h>
 
-void alloc(int proNum, void **pointer, void *value, int isTypeInfo, int expectDeallocProNum, ...);
+#define Alloc(p1, p2) alloc(proNum, (void **)&(p1), (void *)(p2))
+#define Dealloc(p) dealloc(proNum, (void **)&(p))
+
+/* void alloc(int proNum, void **pointer, void *value, int isTypeInfo, int expectDeallocProNum, ...); */
+void alloc(int proNum, void **pointer, void *value);
 void dealloc(int proNum, void **pointer);
 
 #endif
