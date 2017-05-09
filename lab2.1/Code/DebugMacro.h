92a070fe825a117c99093ebc5b6a2ff1e356828e
demons <837940593@qq.com>
2017-05-09 11:41:30 +0000

    .

--- lab2.1/Code/DebugMacro.h
+++ lab2.1/Code/DebugMacro.h
@@ -1,15 +1,11 @@
-#ifndef DebugMacro_H
+#ifndef DebugMarco_H
 #define DebugMacro_H
 
 /* #define flex_debug_print */
-#define syntax_debug_print
+/* #define syntax_debug_print */
 /* #define action_debug_print */
 /* #define function_type_debug_print */
-/* #define type_debug_print */
-/* #define exp_type_debug_print */
-/* #define st_debug_print */
+#define st_debug_print
 /* #define st_stack_debug_print */
-#define pointer_debug
-#define pointer_debug_print
 
 #endif
