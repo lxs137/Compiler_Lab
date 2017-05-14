abc5d43cc2dbd6adf7d6acec1013c76dad74a4fe
demons <837940593@qq.com>
2017-05-14 22:13:35 +0800

    begin to do lab3

---
+++ lab3/Code/syntax_tree.h
@@ -0,0 +1,20 @@
+#ifndef SYNTAX_TREE_H
+#define SYNTAX_TREE_H
+
+typedef struct syntax_tree_node
+{
+    int loc_line;
+    int loc_column;
+    char *str;
+    struct syntax_tree_node *first_child;
+    struct syntax_tree_node *next_brother;
+    int proNum;
+    void *other_info;
+} AST_node;
+
+AST_node *new_token_node(int line, int column, char *string);
+AST_node *new_parent_node(char *string, int proNum, int node_num, ...);
+void clean_up_syntax_tree(AST_node *parent);
+void print_child_node(AST_node *parent, int depth);
+
+#endif
