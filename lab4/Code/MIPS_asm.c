08e52815ff2e232aa96cc8a36080fb98cd48ff78
lxs <lxs137@hotmail.com>
2017-06-20 11:46:10 +0800

    generate asm code

--- lab4/Code/MIPS_asm.c
+++ lab4/Code/MIPS_asm.c
@@ -4,7 +4,8 @@ void test_output()
 {
     asm_add(reg_sp(), reg_sp(), imm(4));
     asm_lw(reg_ra(), addr_im(5));
-    asm_lw(reg("a", 2), addr_im_reg(4, reg_fp()));
+    asm_lw(reg("a", 2), addr_im_reg(imm(4), reg_fp()));
     asm_mv(reg("t", 3), reg("v", 2));
     asm_li(reg("s", 2), imm(4));
+    asm_more_equal(reg("v", 3), reg_0(), 2);
 }
--- lab4/Code/MIPS_asm.h
+++ lab4/Code/MIPS_asm.h
@@ -4,10 +4,10 @@
 #include <stdio.h>
 #include "list.h"
 
-#define p_asm(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#define p_asm(fmt, ...) fprintf(f_output, fmt, ##__VA_ARGS__)
 
 #define imm(val) p_asm("%d", val)
-#define val(val) p_asm("%s", val)
+#define var_name(val) p_asm("%s", val)
 
 #define reg_0() p_asm("$0")
 #define reg_sp() p_asm("$sp")
@@ -19,6 +19,9 @@
 #define addr_reg(reg) p_asm("("),reg,p_asm(")")
 #define addr_im_reg(imm, reg) imm,p_asm("("),reg,p_asm(")") 
 
+#define asm_data_str(var_name, var) var_name,p_asm(": .asciiz \"%s\"\n", var)
+#define asm_data_space(name, size) name,p_asm(": .space %d\n", size)
+
 // add rd, rs, rt
 // rd <-- rs + rt
 #define asm_add(Rdest, Rsrc, rt) \
@@ -26,11 +29,15 @@
 #define asm_sub(Rdest, Rsrc, rt) \
     p_asm("sub "),Rdest,p_asm(", "),Rsrc,p_asm(", "),rt,p_asm("\n")
 #define asm_mul(Rdest, Rsrc, rt) \
-    p_asm("mulo "),Rdest,p_asm(", "),Rsrc,p_asm(", "),rt,p_asm("\n")
+    p_asm("mul "),Rdest,p_asm(", "),Rsrc,p_asm(", "),rt,p_asm("\n")
 #define asm_div(Rdest, Rsrc, rt) \
     p_asm("div "),Rdest,p_asm(", "),Rsrc,p_asm(", "),rt,p_asm("\n")
 #define asm_neg(Rdest, Rsrc) \
     p_asm("neg "),Rdest,p_asm(", -"),Rsrc
+#define asm_and(Rdest, Rsrc, rt) \
+    p_asm("and "),Rdest,p_asm(", "),Rsrc,p_asm(", "),rt,p_asm("\n")
+#define asm_or(Rdest, Rsrc, rt) \
+    p_asm("or "),Rdest,p_asm(", "),Rsrc,p_asm(", "),rt,p_asm("\n")
 
 // sw rt, addr
 // store the word from $rt to address
@@ -46,6 +53,18 @@
 // move rd, rs
 #define asm_mv(Rdest, Rsrc) p_asm("move "),Rdest,p_asm(", "),Rsrc,p_asm("\n")
 
+#define asm_equal(Rs, Rt, no) \
+    p_asm("beq "),Rs,p_asm(", "),Rt,p_asm(", "),p_asm("label%d\n", no)
+#define asm_not_equal(Rs, Rt, no) \
+    p_asm("bne "),Rs,p_asm(", "),Rt,p_asm(", "),p_asm("label%d\n", no)
+#define asm_less(Rs, Rt, no) \
+    p_asm("blt "),Rs,p_asm(", "),Rt,p_asm(", "),p_asm("label%d\n", no)
+#define asm_less_equal(Rs, Rt, no) \
+    p_asm("ble "),Rs,p_asm(", "),Rt,p_asm(", "),p_asm("label%d\n", no)
+#define asm_more(Rs, Rt, no) \
+    p_asm("bgt "),Rs,p_asm(", "),Rt,p_asm(", "),p_asm("label%d\n", no)
+#define asm_more_equal(Rs, Rt, no) \
+    p_asm("bge "),Rs,p_asm(", "),Rt,p_asm(", "),p_asm("label%d\n", no)
 
 FILE *f_output;
 
